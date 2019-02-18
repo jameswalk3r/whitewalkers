1187c93dda3cdd2afae1925ed224911f5177430d
jameswalk3r <jameswalk3r@gmail.com>
2019-02-18 15:25:19 -0600

    Update concerts.sql

--- concerts.sql
+++ concerts.sql
@@ -6,4 +6,8 @@
 
 --INSERT INTO concerts  VALUES (default, '2019-02-02', 'Nashville', 'TN', 'Nasty Smash Birthday Bash');
 
+--CREATE TABLE ticket_holders (id SERIAL PRIMARY KEY, first_name VARCHAR(255), last_name VARCHAR(255), email VARCHAR(255));
+																												   
+--CREATE TABLE tickets (id SERIAL PRIMARY KEY, concert_id SMALLINT REFERENCES concerts(id), ticket_holder_id INTEGER REFERENCES ticket_holders(id));
+
 select * from concerts
