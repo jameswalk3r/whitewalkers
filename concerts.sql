274ea0f910838c281031cd41134b4fa4d1a634b6
jameswalk3r <jameswalk3r@gmail.com>
2019-02-18 15:21:54 -0600

    Create concerts.sql

---
+++ concerts.sql
@@ -0,0 +1,9 @@
+--CREATE TABLE concerts (id SERIAL PRIMARY KEY, DATE DATE, CITY VARCHAR(255), STATE VARCHAR(255), VENUE VARCHAR(255));
+
+--INSERT INTO concerts VALUES (1, '2018-01-01', 'Nashville', 'TN', 'Ryman');
+
+--select * from concerts
+
+--INSERT INTO concerts  VALUES (default, '2019-02-02', 'Nashville', 'TN', 'Nasty Smash Birthday Bash');
+
+select * from concerts
