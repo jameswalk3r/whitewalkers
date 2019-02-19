0679fd302c07bc5b89926284a2f2b4dd5b08ede9
Oliver <white_oliver@comcast.net>
2019-02-18 22:58:01 -0600

    Update OliverCode.sql

--- OliverCode.sql
+++ OliverCode.sql
@@ -1,1 +1,19 @@
 code
+--select * from tickets;
+--select * from ticket_holders;
+--select * from concerts;
+--INSERT INTO concerts VALUES (default, '2019-02-18', 'Murfreesboro', 'TN', 'Walker House');
+--BEGIN TRANSACTION;
+--ALTER TABLE concerts DROP COLUMN test;
+--COMMIT TRANSACTION;
+--ROLLBACK TRANSACTION;
+--select * from concerts;
+--ALTER TABLE concerts ALTER COLUMN country SET DEFAULT 'USA';
+BEGIN TRANSACTION;
+UPDATE concerts SET country = 'Mexico' WHERE city = 'Murfreesboro';
+select * from concerts;
+
+
+
+
+
