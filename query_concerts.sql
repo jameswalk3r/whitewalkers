8161a4454613eae7d49849876d49e637d35a99df
James Walker <jameswalk3r@gmail.com>
2019-02-18 23:35:10 -0600

    Updated cases for keywords to all uppercase

--- query_concerts.sql
+++ query_concerts.sql
@@ -1,13 +1,13 @@
 --Report per Jon Doe request
 --2018-02-18
-select 
+SELECT 
 	venue,
 	date,
 	city,
 	state,
 	country
-from concerts
+FROM concerts
 --He only wanted to see venues from this year in the USA
-where date >= '2019-01-01'
-and country = 'USA'
+WHERE DATE >= '2019-01-01'
+AND country = 'USA'
 --you suck
